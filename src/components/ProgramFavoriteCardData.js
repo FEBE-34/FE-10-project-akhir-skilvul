450a53d1a3faf34e7bfe2695884236eb1776e04f
salsa <salsaprmudta@gmail.com>
2022-11-23 17:08:04 +0700

    commit

---
+++ src/components/ProgramFavoriteCardData.js
@@ -0,0 +1,27 @@
+import program1 from "../assets/img/program-fav1.png"
+import program2 from "../assets/img/program-fav2.png"
+import program3 from "../assets/img/program-fav3.png"
+
+const ProgramFavoriteCardData= [
+    {
+        imgsrc: program1,
+        title : "Program Bantuan Kitabisa.com",
+        text : "Program Bantuan pasca pandemi oleh pt kitabisa.com",
+        view : "",
+    },
+
+    {
+        imgsrc: program2,
+        title : "Lowongan Pekerjaan PT. Nusa tbk",
+        text : "lowongan pekerjaan karyawan tetap oleh PT. Nusa tbk",
+        view : "",
+    },
+    {
+        imgsrc: program3,
+        title : "Webinar Mental Health Pt. Neo ",
+        text : "Webinar kepedulian terhadap mental health",
+        view : "",
+    },
+    
+]
+export default ProgramFavoriteCardData
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import HeroImg from '../components/HeroImg'
+import ProgramFavorite from '../components/ProgramFavorite'
+
+
+
+
+function HomePage() {
+  return (
+    <div>
+    <HeroImg />
+ 
+    <ProgramFavorite />
+
+  
+    </div>
+  )
+}
+
+export default HomePage
