8caa883f6ddf95cfc61a118b316638c84a4e4f86
Muhammad Sadewo Wicaksono <114298461+musadewo@users.noreply.github.com>
2022-11-23 11:00:00 +0700

    Navbar kelar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import ForgotPassword from "./pages/ForgotPassword";
 import LandingPage from "./pages/LandingPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/signin" element={<Signin />} />
